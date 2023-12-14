789ef1760c98e97f1b91564d695eb3336ac8a23a
Nikolay Skripko <nskripko@icloud.com>
2023-12-14 19:17:28 +0300

    bug fixes
    The primary key for the participation table has been added

--- task03.sql
+++ task03.sql
@@ -3,7 +3,7 @@
 CREATE SCHEMA IF NOT EXISTS db_project;
 SET search_path = db_project, public;
 
-DROP TABLE IF EXISTS Player CASCADE;
+-- DROP TABLE IF EXISTS Player CASCADE;
 CREATE TABLE IF NOT EXISTS Player (
     player_id SERIAL PRIMARY KEY,
     nickname TEXT,
@@ -13,11 +13,11 @@ CREATE TABLE IF NOT EXISTS Player (
     CONSTRAINT address_format CHECK (address LIKE '%,%,%,%')
 );
 
-DROP TABLE Blacklist;
+-- DROP TABLE Blacklist;
 CREATE TABLE IF NOT EXISTS Blacklist (
     blacklist_id SERIAL PRIMARY KEY,
     ban_reason TEXT,
-    ban_date TIMESTAMP(0) NOT NULL DEFAULT NOW()::timestamp(0),
+    ban_date TIMESTAMP NOT NULL DEFAULT NOW()::timestamp,
     player_id INTEGER,
     CONSTRAINT fk_blacklist 
     	FOREIGN KEY (player_id) 
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS Blacklist (
     			ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS Game_Description CASCADE;
+-- DROP TABLE IF EXISTS Game_Description CASCADE;
 CREATE TABLE IF NOT EXISTS Game_Description (
     game_id SERIAL PRIMARY KEY,
     game_name TEXT,
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS Game_Description (
     CONSTRAINT valid_hall CHECK (hall IN ('Red', 'Green', 'Blue', 'Black', 'Yellow'))
 );
 
-DROP TABLE IF EXISTS Gambling_Table CASCADE;
+-- DROP TABLE IF EXISTS Gambling_Table CASCADE;
 CREATE TABLE IF NOT EXISTS Gambling_Table (
 	table_id SERIAL PRIMARY KEY,
 	out_of_order BOOLEAN DEFAULT FALSE,
@@ -46,19 +46,19 @@ CREATE TABLE IF NOT EXISTS Gambling_Table (
 				ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS Event_Table CASCADE;
+-- DROP TABLE IF EXISTS Event_Table CASCADE;
 CREATE TABLE IF NOT EXISTS Event_Table (
 	event_id SERIAL PRIMARY KEY,
 	table_id INTEGER,
-	date_start TIMESTAMP(0) NOT NULL DEFAULT NOW()::timestamp(0),
-	date_end TIMESTAMP(0) NOT NULL DEFAULT '3000-01-01 23:09:00'::timestamp(0),
+	date_start TIMESTAMP NOT NULL DEFAULT NOW()::timestamp,
+	date_end TIMESTAMP NOT NULL DEFAULT '3000-01-01 23:09:00',
 	CONSTRAINT fk_event_table
 		FOREIGN KEY (table_id)
 			REFERENCES Gambling_Table(table_id)
 				ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS Participation CASCADE;
+-- DROP TABLE IF EXISTS Participation CASCADE;
 CREATE TABLE IF NOT EXISTS Participation (
 	participation_id SERIAL PRIMARY KEY,
 	player_id INTEGER,
@@ -73,7 +73,7 @@ CREATE TABLE IF NOT EXISTS Participation (
 				ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS Drink_Info CASCADE;
+-- DROP TABLE IF EXISTS Drink_Info CASCADE;
 CREATE TABLE IF NOT EXISTS Drink_Info (
 	drink_id SERIAL PRIMARY KEY,
 	drink_title TEXT,
@@ -81,18 +81,18 @@ CREATE TABLE IF NOT EXISTS Drink_Info (
 	drink_price INTEGER NOT NULL
 );
 
-DROP TABLE IF EXISTS Order_Table CASCADE;
+-- DROP TABLE IF EXISTS Order_Table CASCADE;
 CREATE TABLE IF NOT EXISTS Order_Table (
 	order_id SERIAL PRIMARY KEY,
 	player_id INTEGER,
-	order_date TIMESTAMP(0) NOT NULL DEFAULT NOW()::timestamp(0),
+	order_date TIMESTAMP NOT NULL DEFAULT NOW()::timestamp,
 	CONSTRAINT fk_order_table
 		FOREIGN KEY (player_id)
 			REFERENCES Player(player_id)
 				ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS Sale CASCADE;
+-- DROP TABLE IF EXISTS Sale CASCADE; 
 CREATE TABLE IF NOT EXISTS Sale (
 	quantity INTEGER NOT NULL DEFAULT 1,
 	drink_id INTEGER,
@@ -107,11 +107,13 @@ CREATE TABLE IF NOT EXISTS Sale (
 				ON DELETE CASCADE
 );
 
-DROP TABLE Chip_Transaction;
+
+-- DROP TABLE Chip_Transaction CASCADE;
 CREATE TABLE IF NOT EXISTS Chip_Transaction (
+	transaction_id SERIAL PRIMARY KEY,
 	type_of_the_transaction TEXT,
 	amount DECIMAL NOT NULL,
-	transaction_date TIMESTAMP(0) NOT NULL DEFAULT NOW()::timestamp(0),
+	transaction_date TIMESTAMP NOT NULL DEFAULT NOW()::timestamp,
 	player_id INTEGER,
 	participation_id INTEGER,
 	order_id INTEGER,
@@ -127,7 +129,7 @@ CREATE TABLE IF NOT EXISTS Chip_Transaction (
 		FOREIGN KEY (order_id)
 			REFERENCES Order_Table(order_id)
 				ON DELETE CASCADE,
-	CONSTRAINT valid_type CHECK (type_of_the_transaction IN ('Bar', 'Game', 'Chip'))
+	CONSTRAINT valid_type CHECK (type_of_the_transaction IN ('Bar', 'Game', 'Chip', 'Game_Result'))
 );
 
 
