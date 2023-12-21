02a4e63c6a222b5af82014eaeee97df5ec24b9fd
Nadezhda Igolkina <nad.igolki@gmail.com>
2023-12-21 03:12:40 +0300

    task09 - fix

--- sql/task09.sql
+++ sql/task09.sql
@@ -1,5 +1,7 @@
--- Debt-triggered blacklist addition
-create or replace function auto_blacklist_fordebt()
+-- Отмена операции описания фишек, если:
+-- 1. Участник ЗАБАНЕН
+-- 2. Участник уходит в МИНУС
+create or replace function check_transaction()
 returns trigger as $$
 declare
   total_amount int;
@@ -8,31 +10,34 @@ begin
 
   select exists(select 1 from blacklist where player_id = new.player_id) into is_blacklisted;
 
-  if new.amount < 0 and not is_blacklisted then
+  if is_blacklisted then
+    raise notice 'Участник с player_id % ЗАБАНЕН. Транзакция невозможна.', new.player_id;
+    return null;
+  end if;
+  if new.amount < 0 then
       select sum(ct.amount)
   	  into total_amount
       from player p
       join chip_transaction ct on p.player_id = ct.player_id
       where p.player_id = new.player_id;
-     if total_amount + new.amount < -10000 then
-      raise notice 'Участник с player_id % забанен.', new.player_id;
-      insert into blacklist (player_id, ban_reason, ban_date)
-      values (new.player_id, 'Debt exceeding limit', current_date);
+     if total_amount + new.amount < 0 then
+      raise notice 'У участника с player_id % не хватает фишек.', new.player_id;
+       return null;
      end if;
-
+  return new;
   end if;
 
   return new;
 end;
 $$ language plpgsql;
 
-create or replace trigger auto_blacklist_fordebt_trigger
-after insert on chip_transaction
+create or replace trigger check_transaction_trigger
+before insert on chip_transaction
 for each row
-execute function auto_blacklist_fordebt();
+execute function check_transaction();
 
 
--- Сhecking the player is not blacklisted
+-- Отмена операции добавления в игру, если участник ЗАБАНЕН
 create or replace function check_blacklist()
 returns trigger as $$
 begin
@@ -41,7 +46,7 @@ begin
         from blacklist
         where player_id = new.player_id
     ) then
-    	raise notice 'Участник с player_id % в черном списке. Участие невозможно.', new.player_id;
+    	raise notice 'Участник с player_id % ЗАБАНЕН. Участие невозможно.', new.player_id;
         return null;
     end if;
 
